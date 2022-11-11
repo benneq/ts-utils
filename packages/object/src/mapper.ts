a88a8c6fd0452dfebbb15621acd0ea9279c2c042
Ben M <1627305+benneq@users.noreply.github.com>
2022-11-11 02:10:30 +0100

    object mapper typings

--- packages/object/src/mapper.ts
+++ packages/object/src/mapper.ts
@@ -4,7 +4,9 @@ import { isFunction, Mapper } from "@benneq/function";
 type Primitive = string | null | number | boolean | undefined | bigint | symbol;
 type DefFunction<I, T> = (value: I) => T;
 type DefArray<I, T> = ReadonlyArray<MapperDefinition<I, T>>;
-type DefObject<I, T> = Readonly<{ [key in keyof T]: MapperDefinition<I, T[key]> }>;
+type DefObject<I, T> = {
+  [key in keyof T]: MapperDefinition<I, T[key]>;
+};
 
 type MapperDefinition<I, T> = T extends Primitive
   ? DefFunction<I, T>
@@ -47,9 +49,11 @@ export const mapper =
     } else if (isArray(def)) {
       return def.map((e) => mapper(e)(initialValue)) as T;
     } else {
-      return Object.entries(def).reduce((acc, [key, value]: [string, any]) => {
+      return (
+        Object.entries(def) as [keyof T, MapperDefinition<I, T[keyof T]>][]
+      ).reduce((acc, [key, value]) => {
         acc[key] = mapper(value)(initialValue);
         return acc;
-      }, {} as Record<string, unknown>) as T;
+      }, {} as T);
     }
   };
