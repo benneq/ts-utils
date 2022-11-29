299826fbcf2b07c1ecdf5e11c3864daa8be0214a
Ben M <1627305+benneq@users.noreply.github.com>
2022-11-29 03:43:30 +0100

    collections MultiMap

--- packages/collections/src/_types.ts
+++ packages/collections/src/_types.ts
@@ -1,31 +1,124 @@
 import { Entry } from "@benneq/object";
 
+/**
+ * A {@link ReadonlyMultiMap} can have multiple values per key.
+ *
+ * @typeParam K - the key type
+ * @typeParam V - the value type
+ */
 export interface ReadonlyMultiMap<K, V> extends Iterable<Entry<K, V>> {
+  /**
+   * @see {@link Map.forEach}
+   */
   forEach(callbackfn: (value: V, key: K, multiMap: this) => void): void;
 
+  /**
+   * Gets all values for this key.
+   *
+   * Returns an empty iterable if the key does not exist.
+   *
+   * @param key - the key to look up
+   * @returns an {@link Iterable} containing all values for this key
+   */
   get(key: K): Iterable<V>;
 
-  has(key: K, value?: V): boolean;
+  /**
+   * Checks if this {@link MultiMap} contains an entry for this key.
+   *
+   * @see {@link Map.has}
+   *
+   * @param key - the key to look up
+   * @returns `true` if an entry with the given key exists, otherwise `false`
+   */
+  has(key: K): boolean;
 
+  /**
+   * Checks if this {@link MultiMap} contains an entry with the key and value.
+   *
+   * @see {@link Map.has}
+   *
+   * @param key - the key to look up
+   * @param value - the value to look for
+   * @returns `true` if an entry with the given key and value exists, otherwise `false`
+   */
+  has(key: K, value: V): boolean;
+
+  /**
+   * @returns the number of entries in this {@link MultiMap}
+   */
   readonly size: number;
 
+  /**
+   * @see {@link Map.entries}
+   */
   entries(): IterableIterator<[K, V]>;
 
+  /**
+   * @see {@link Map.keys}
+   */
   keys(): IterableIterator<K>;
 
+  /**
+   * @see {@link Map.forEach}
+   */
   values(): IterableIterator<V>;
 
   [Symbol.toStringTag]: string;
 }
 
+/**
+ * A {@link MultiMap} can have multiple values per key.
+ *
+ * @typeParam K - the key type
+ * @typeParam V - the value type
+ */
 export interface MultiMap<K, V> extends ReadonlyMultiMap<K, V> {
+  /**
+   * Removes all entries from this {@link MultiMap}.
+   */
   clear(): void;
 
-  delete(key: K, value?: V): boolean;
+  /**
+   * Deletes the key with all its values.
+   *
+   * @param key - the key to delete
+   * @returns `true` if an entry was deleted, otherwise `false`
+   */
+  delete(key: K): boolean;
 
+  /**
+   * Deletes the first entry with the given `key` and `value`.
+   *
+   * @param key - the key to look for
+   * @param value - the value to delete
+   * @returns `true` if an entry was deleted, otherwise `false`
+   */
+  delete(key: K, value: V): boolean;
+
+  /**
+   * Set the values for a key and replaces all previous values for this key.
+   *
+   * If no values are provided all previous entries will be removed.
+   *
+   * @param key - the key to set
+   * @param value - the values to set
+   * @returns this {@link MultiMap}
+   */
   set(key: K, values: Iterable<V>): this;
 
+  /**
+   * Adds a value for a key to this {@link MultiMap}.
+   *
+   * @param key - the key to look up
+   * @param value - the value to add
+   * @returns this {@link MultiMap}
+   */
   add(key: K, value: V): this;
 
+  /**
+   * Adds all entries to this {@link MultiMap}.
+   *
+   * @param entries - the entries to add
+   */
   addAll(entries: Iterable<Entry<K, V>>): void;
 }
--- packages/collections/src/abstractMultiMap.ts
+++ packages/collections/src/abstractMultiMap.ts
@@ -1,3 +1,4 @@
+import { identity } from "@benneq/function";
 import { count, flatMap, map, some } from "@benneq/iterable";
 import { Entry } from "@benneq/object";
 import { MultiMap } from "./_types";
@@ -20,75 +21,23 @@ export abstract class AbstractMultiMap<K, V> implements MultiMap<K, V> {
     this.#value = map;
   }
 
-  /**
-   * @see {@link Map.clear}
-   */
   clear(): void {
     this.#value.clear();
   }
 
-  /**
-   * Deletes the key with all its values.
-   *
-   * @see {@link Map.delete}
-   *
-   * @param key - the key to delete
-   * @returns `true` if an entry was deleted, otherwise `false`
-   */
   abstract delete(key: K): boolean;
 
-  /**
-   * Deletes the first entry with the given `key` and `value`.
-   *
-   * @see {@link Map.delete}
-   *
-   * @param key - the key to look for
-   * @param value - the value to delete
-   * @returns `true` if an entry was deleted, otherwise `false`
-   */
   abstract delete(key: K, value: V): boolean;
 
-  /**
-   * @see {@link Map.forEach}
-   */
   forEach(callbackfn: (value: V, key: K, multiMap: this) => void): void {
     for (const [key, value] of this) {
       callbackfn(value, key, this);
     }
   }
 
-  /**
-   * Gets all values for this key.
-   *
-   * Returns an empty iterable if the key does not exist.
-   *
-   * @param key - the key to look up
-   * @returns an {@link Iterable} containing all values for this key
-   */
   get(key: K): Iterable<V> {
     return this.#value.get(key) || [];
   }
-
-  /**
-   * Checks if this {@link MultiMap} contains an entry for this key.
-   *
-   * @see {@link Map.has}
-   *
-   * @param key - the key to look up
-   * @returns `true` if an entry with the given key exists, otherwise `false`
-   */
-  has(key: K): boolean;
-
-  /**
-   * Checks if this {@link MultiMap} contains an entry with the key and value.
-   *
-   * @see {@link Map.has}
-   *
-   * @param key - the key to look up
-   * @param value - the value to look for
-   * @returns `true` if an entry with the given key and value exists, otherwise `false`
-   */
-  has(key: K, value: V): boolean;
 
   has(key: K, value?: V): boolean {
     return (
@@ -97,64 +46,32 @@ export abstract class AbstractMultiMap<K, V> implements MultiMap<K, V> {
     );
   }
 
-  /**
-   * Set the values for a key and replaces all previous values for this key.
-   *
-   * If no values are provided all previous entries will be removed.
-   *
-   * @see {@link Map.set}
-   *
-   * @param key - the key to set
-   * @param value - the values to set
-   * @returns this {@link AbstractMultiMap}
-   */
   abstract set(key: K, values: Iterable<V>): this;
 
-  /**
-   * Adds a value for a key to this {@link AbstractMultiMap}.
-   *
-   * @param key - the key to look up
-   * @param value - the value to add
-   * @returns this {@link AbstractMultiMap}
-   */
   abstract add(key: K, value: V): this;
 
   addAll(entries: Iterable<Entry<K, V>>): void {
-    if (entries) {
-      for (const [key, value] of entries) {
-        this.add(key, value);
-      }
+    for (const [key, value] of entries) {
+      this.add(key, value);
     }
   }
 
-  /**
-   * @returns the number of entries in this {@link MultiMap}
-   */
   get size(): number {
     return count(this) || 0;
   }
 
-  /**
-   * @see {@link Map.entries}
-   */
   entries(): IterableIterator<[K, V]> {
     return flatMap<[K, Iterable<V>], [K, V]>(([key, values]) =>
       map<V, [K, V]>((value) => [key, value])(values)
     )(this.#value);
   }
 
-  /**
-   * @see {@link Map.keys}
-   */
   keys(): IterableIterator<K> {
     return this.#value.keys();
   }
 
-  /**
-   * @see {@link Map.forEach}
-   */
   values(): IterableIterator<V> {
-    return map<[K, V], V>(([_key, value]) => value)(this);
+    return flatMap<Iterable<V>, V>(identity)(this.#value.values());
   }
 
   [Symbol.iterator](): IterableIterator<[K, V]> {
